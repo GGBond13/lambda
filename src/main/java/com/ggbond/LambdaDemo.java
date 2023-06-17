5f0dcc6bcba43f29930570d319a2b7a580ef043d
GGBond <352266375@qq.com>
2023-06-17 21:24:52 +0800

    Initial commit

---
+++ src/main/java/com/ggbond/LambdaDemo.java
@@ -0,0 +1,8 @@
+package com.ggbond;
+//复习Lambda Express
+//使用Lambda Express前提是它是一个函数接口
+//小口诀:拷贝小括号,写死右箭头,落地大括号
+@FunctionalInterface
+public interface LambdaDemo {
+    public void sayHello();
+}
---
+++ src/main/java/com/ggbond/LambdaText01.java
@@ -0,0 +1,16 @@
+package com.ggbond;
+
+public class LambdaText01 {
+    public static void main(String[] args) {
+        //匿名内部类
+     /*LambdaDemo lambdaDemo = new LambdaDemo() {
+         @Override
+         public void sayHello() {
+             System.out.println("Hello,Lambda!");
+         }
+     };*/
+     //Lambda表达式转换,小口诀:拷贝小括号,写死右箭头,落地大括号
+     LambdaDemo lambdaDemo = () -> {System.out.println("Hello,Lambda!");};
+     lambdaDemo.sayHello();
+    }
+}
